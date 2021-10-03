56f31ab9a2c5e1648230bbd3d95602bfc34a80b3
Andrew <Andrey18651@gmail.com>
2021-10-03 18:02:37 +0300

    add Statistic model

--- api/models.py
+++ api/models.py
@@ -1,1 +1,39 @@
+from typing import Optional
+
 from django.db import models
+from djmoney.models.fields import MoneyField
+
+
+class Statistic(models.Model):
+
+    def __str__(self):
+        return f'Статистика № {self.id} за дату {self.date}'
+
+    date = models.DateField()
+    views = models.IntegerField(
+        blank=True,
+        null=True
+    )
+    clicks = models.IntegerField(
+        blank=True,
+        null=True
+    )
+    cost = MoneyField(
+        max_digits=14,
+        decimal_places=2,
+        default_currency='RUB',
+        blank=True,
+        null=True
+    )
+
+    @property
+    def cpc(self) -> Optional[float]:
+        cost = self.cost
+        clicks = self.clicks
+        return cost / clicks if cost and clicks else None
+
+    @property
+    def cpm(self) -> Optional[float]:
+        cost = self.cost
+        views = self.views
+        return cost / views * 1000 if cost and views else None
